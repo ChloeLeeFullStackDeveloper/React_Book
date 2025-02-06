1938a9d743e69ac5f9ef67ab304c0e4580898a07
Chloe Lee <itprofessional.4ever7@gmail.com>
2025-02-06 09:40:09 -0700

    create book

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import BookCreate from './components/BookCreate';
 
 function App() {
+  const [books, setBooks] = useState([]);
+  
+  const createBook = (title) => {
+    console.log('Need to add book with:', title);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>React Starter Project</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.js</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  )
 }
 
 export default App;
